8367b7b91a36fa69e5df0d7c16520482af1548a3
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-02 21:21:45 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -27,5 +27,5 @@ if '%errorlevel%' NEQ '0' (
 :--------------------------------------
 :: your commands begin from this point.
 :: stops the service and then starts it 
-net stop EmiliaAnimeBot
-net start EmiliaAnimeBot
+net stop NarutoAnimeBot
+net start NarutoAnimeBot
