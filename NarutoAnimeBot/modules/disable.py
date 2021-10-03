981ee0fea879f70769c460fe0761333c650fb2ce
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 13:54:13 +0530

    Update disable.py

--- NarutoAnimeBot/modules/disable.py
+++ NarutoAnimeBot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from EmiliaAnimeBot import dispatcher
-from EmiliaAnimeBot.modules.helper_funcs.handlers import (CMD_STARTERS)
-from EmiliaAnimeBot.modules.helper_funcs.misc import is_module_loaded
+from NarutoAnimeBot import dispatcher
+from NarutoAnimeBot.modules.helper_funcs.handlers import (CMD_STARTERS)
+from NarutoAnimeBot.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, RegexHandler)
@@ -15,9 +15,9 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from EmiliaAnimeBot.modules.helper_funcs.chat_status import (
+    from NarutoAnimeBot.modules.helper_funcs.chat_status import (
         connection_status, is_user_admin, user_admin)
-    from EmiliaAnimeBot.modules.sql import disable_sql as sql
+    from NarutoAnimeBot.modules.sql import disable_sql as sql
     from telegram.ext.dispatcher import run_async
 
     DISABLE_CMDS = []
