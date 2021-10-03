819c1509191f02cfcba155255938b92b135ac838
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 14:30:18 +0530

    Update qr.py

--- NarutoAnimeBot/modules/qr.py
+++ NarutoAnimeBot/modules/qr.py
@@ -9,8 +9,8 @@ from bing_image_downloader import downloader
 from requests import get, post
 from telethon.tl import functions, types
 
-from EmiliaAnimeBot import telethn as client
-from EmiliaAnimeBot.events import register
+from NarutoAnimeBot import telethn as client
+from NarutoAnimeBot.events import register
 
 
 def progress(current, total):
