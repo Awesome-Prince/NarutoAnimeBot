783383997ed95997e90a1ca20723bbb3ad510966
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 14:34:56 +0530

    Update ping.py

--- NarutoAnimeBot/modules/ping.py
+++ NarutoAnimeBot/modules/ping.py
@@ -5,9 +5,9 @@ import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from EmiliaAnimeBot import StartTime, dispatcher
-from EmiliaAnimeBot.modules.helper_funcs.chat_status import sudo_plus
-from EmiliaAnimeBot.modules.disable import DisableAbleCommandHandler
+from NarutoAnimeBot import StartTime, dispatcher
+from NarutoAnimeBot.modules.helper_funcs.chat_status import sudo_plus
+from NarutoAnimeBot.modules.disable import DisableAbleCommandHandler
 
 sites_list = {
     "Telegram": "https://api.telegram.org",
