6e1b90a8fc5276dc1555573423bc4de5060062ed
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 11:46:52 +0530

    Update approve_sql.py

--- NarutoAnimeBot/modules/sql/approve_sql.py
+++ NarutoAnimeBot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from EmiliaAnimeBot.modules.sql import BASE, SESSION
+from NarutoAnimeBot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
