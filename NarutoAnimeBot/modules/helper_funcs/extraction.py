aca8ce451277af6128dde13720786fdb6abfa5df
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 11:38:30 +0530

    Update extraction.py

--- NarutoAnimeBot/modules/helper_funcs/extraction.py
+++ NarutoAnimeBot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from EmiliaAnimeBot import LOGGER
-from EmiliaAnimeBot.modules.users import get_user_id
+from NarutoAnimeBot import LOGGER
+from NarutoAnimeBot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
