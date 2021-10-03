416bb85d9ad368d38ca710c1fd1fddd707a85769
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 13:25:44 +0530

    Update locks_sql.py

--- NarutoAnimeBot/modules/sql/locks_sql.py
+++ NarutoAnimeBot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from EmiliaAnimeBot.modules.sql import SESSION, BASE
+from NarutoAnimeBot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
