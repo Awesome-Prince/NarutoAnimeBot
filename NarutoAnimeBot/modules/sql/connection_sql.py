79c5f1164bff9d5a00dd441691f5eae44922eaec
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 13:22:58 +0530

    Update connection_sql.py

--- NarutoAnimeBot/modules/sql/connection_sql.py
+++ NarutoAnimeBot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from EmiliaAnimeBot.modules.sql import SESSION, BASE
+from NarutoAnimeBot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
