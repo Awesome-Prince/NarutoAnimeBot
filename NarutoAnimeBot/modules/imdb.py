69b3160e35bd0a4e05432c91be8e4334831f97df
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 14:46:25 +0530

    Update imdb.py

--- NarutoAnimeBot/modules/imdb.py
+++ NarutoAnimeBot/modules/imdb.py
@@ -1,11 +1,11 @@
-from EmiliaAnimeBot import telethn as tbot
+from NarutoAnimeBot import telethn as tbot
 import os
 import re
 import bs4
 import requests
 from telethon import types
 from telethon.tl import functions
-from EmiliaAnimeBot.events import register
+from NarutoAnimeBot.events import register
 
 langi = "en"
 
