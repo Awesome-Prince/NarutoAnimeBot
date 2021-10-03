448b8c53acdc4e2e34a5019f9f80ecd49d0a0939
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 11:47:30 +0530

    Update blacklist_sql.py

--- NarutoAnimeBot/modules/sql/blacklist_sql.py
+++ NarutoAnimeBot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from EmiliaAnimeBot.modules.sql import SESSION, BASE
+from NarutoAnimeBot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
