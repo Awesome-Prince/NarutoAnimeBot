ac57423bab8da2108b697df9a411506d8417d285
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-10-03 13:24:58 +0530

    Update feds_sql.py

--- NarutoAnimeBot/modules/sql/feds_sql.py
+++ NarutoAnimeBot/modules/sql/feds_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from EmiliaAnimeBot import dispatcher
-from EmiliaAnimeBot.modules.sql import BASE, SESSION
+from NarutoAnimeBot import dispatcher
+from NarutoAnimeBot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 from telegram.error import BadRequest, Unauthorized
 
