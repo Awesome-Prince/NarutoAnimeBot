4a6216acdcaf1cb337f65045b20bc065371216a3
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-10-12 16:31:35 +0530

    Update __main__.py

--- NarutoAnimeBot/__main__.py
+++ NarutoAnimeBot/__main__.py
@@ -701,7 +701,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Naruto IS BACK TO ONLINE")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "₱Ø₭Ɇ ⱤØ฿Ø₮ ✗LY IS BACK TO ONLINE")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
